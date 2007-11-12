68468696906860ff35520d4f9a4917e8740cdc3c
brian <brian@1fda6f16-4416-0410-bfd6-867a04880151>
2007-11-12 05:22:30 +0000

    Gratia additions (old stuff, never committed).
    git-svn-id: svn://t2.unl.edu/brian/GraphUsers/gratia@1340 1fda6f16-4416-0410-bfd6-867a04880151

--- src/gratia/graphs/gratia_graphs.py
+++ src/gratia/graphs/gratia_graphs.py
@@ -3,6 +3,10 @@ from graphtool.graphs.graph import DBGraph, TimeGraph
 from graphtool.graphs.common_graphs import StackedBarGraph, BarGraph, CumulativeGraph, PieGraph, QualityMap
 import types
 
+from graphtool.graphs.graph import prefs
+
+prefs['watermark'] = '$CONFIG_ROOT/osg_logo_4c_white.png'
+
 class GratiaStackedBar( TimeGraph, StackedBarGraph ):
   pass
 
