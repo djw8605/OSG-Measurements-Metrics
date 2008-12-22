8f7c8cfcce81c77ecf5246040e2eaec9abdb02c7
brian <brian@1fda6f16-4416-0410-bfd6-867a04880151>
2008-12-22 14:51:56 +0000

    Corrections to the setup scripts.
    git-svn-id: svn://t2.unl.edu/brian/GraphUsers/gratia@2651 1fda6f16-4416-0410-bfd6-867a04880151

---
+++ setup/setup_basic.py
@@ -0,0 +1,46 @@
+
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(name="AccountingView",
+      version="0.1",
+      author="Brian Bockelman",
+      author_email="bbockelm@math.unl.edu",
+      url="http://t2.unl.edu/documentation/gratia_graphs",
+      description="Python-based Gratia Viewer.",
+
+      package_dir={"": "src"},
+      packages=find_packages("src"),
+      include_package_data=True,
+
+      classifiers = [
+          'Development Status :: 2 - Pre-Alpha',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: POSIX'
+      ],
+     
+      entry_points={
+          'console_scripts': [
+              'gratia_basic_web_dev = gratia.tools.gratia_basic_web_dev:main',
+              'gratia_basic_web = gratia.tools.gratia_basic_web:main',
+              'static_graphs = gratia.tools.static_graphs:main',
+              'make_daemon_gratia = gratia.other.make_daemon:main',
+          ],
+          'setuptools.installation' : [
+              'eggsecutable = gratia.tools.gratia_web:main'
+          ]
+      },
+
+      data_files=[
+          ('/etc/init.d', ['config/GratiaBasicWeb']),
+          ('/etc/', ['config/DBParam.xml.rpmnew']),
+      ],
+
+      namespace_packages = ['gratia']
+      )
+
+
