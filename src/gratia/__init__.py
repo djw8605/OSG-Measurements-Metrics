a18bad81d5c89b6b9a372098ab3113b4e3763595
brian <brian@1fda6f16-4416-0410-bfd6-867a04880151>
2009-02-06 22:01:30 +0000

    Amanda's fix for the text box.
    git-svn-id: svn://t2.unl.edu/brian/GraphUsers/gratia@2807 1fda6f16-4416-0410-bfd6-867a04880151

--- src/gratia/__init__.py
+++ src/gratia/__init__.py
@@ -2,4 +2,4 @@ import warnings
 warnings.filterwarnings('ignore', 'Could not find matplotlibrc; using defaults')
 warnings.filterwarnings('ignore', 'could not find rc file; returning defaults')
 
-__import__('pkg_resources').declare_namespace(__name__)
+#__import__('pkg_resources').declare_namespace(__name__)
