84c440b9cb6f26e2ad7f79b9b14675779802b6db
aguru <aguru@1fda6f16-4416-0410-bfd6-867a04880151>
2011-09-19 22:04:44 +0000

    Added the confog for missing CSV handler
    git-svn-id: svn://t2.unl.edu/brian/GraphUsers/gratia@4525 1fda6f16-4416-0410-bfd6-867a04880151

--- src/gratia/web/jot_reporting.py
+++ src/gratia/web/jot_reporting.py
@@ -101,7 +101,7 @@ class JOTReporter(Authenticate):
         for key in alice_pledge:
             data['mou'][key] = days_in_month*.67*24*int(alice_pledge[key]['pledge'])
         for resource, fed in federations.items():
-            print "TRACE Resource %s associated with federation %s with wall hours %s. Norm factor %s " % (resource, fed, wall_hours.get(resource, 0)*norms[resource], norms[resource])
+            print "TRACE Resource %s associated with federation %s, resource %s with wall hours %s. Norm factor %s " % (resource, fed, resource, wall_hours.get(resource, 0)*norms[resource], norms[resource])
             #data['reliability'].setdefault(fed, 0)
             #data['reliability'][fed] +=resource_reliability.get(resource, {}).\
             #    get(datetime.datetime(year, month, 1, 0, 0, 0), (0, ))[0]
